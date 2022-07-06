e2eec0825321eae8064a5ce9f4d938dc0e34da8d
pogilvie <code@ogilvie.us.com>
2022-07-06 15:01:53 -0700

    start

---
+++ client.ts
@@ -0,0 +1,15 @@
+import got from 'got';
+
+
+async function main() {
+    const { data } = await got.post('https://httpbin.org/anything', {
+        json: {
+            hello: 'world'
+        }
+    }).json();
+    
+    console.log(data);
+    //=> {"hello": "world"}
+}
+
+main();
